4031cfa62fef288a289117df92519d36215eb130
sairamreddy1 <sairam77310@gmail.com>
2025-11-04 19:33:02 -0600

    vhanges done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders game title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Lewis-Tac-Azure by sai ram reddy/i);
+  expect(heading).toBeInTheDocument();
 });
